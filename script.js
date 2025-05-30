bccbc4fe7c76a48088f47755ac3d2646e5b8bedf
JevonGalang <168asal168@gmail.com>
2025-05-30 09:56:19 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var button = document.getElementById("buttondesc")
+button.style.backgroundColor = "blue"
+button.style.color = "white"
+button.style.borderRadius = "8px"
+button.style.height = "37px"
+
+function btndesc1() {
+    const pesan = document.getElementById("deskripsi")
+    pesan.textContent = "Spesfikasi RAM 16GB, SSD 512 , Layar 15.6"
+}
